636a78cd4e21c219885660375d0af6f0d2e11cb3
Abhishek Tilwar <abhishektilwar@gmail.com>
2022-12-18 10:59:48 +0530

    Create finding item.cpp

---
+++ finding item.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    // item n
+    int n, num;
+    cin>>n>>num;
+    for(int i=0; i<n; i++){
+        int t;
+        cin>>t;
+        if(t==num){
+            cout<<"item matched!";
+        }
+            
+    }
+    
+    return 0;
+}
